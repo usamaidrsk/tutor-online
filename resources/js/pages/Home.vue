459f6f5b2c42bdffb6f5420705e96a6fd2893bee
robinhood05 <robinhood0593@gmail.com>
2020-04-18 13:25:38 -0400

    Add `Home.vue` page

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="hero text--center">
+            <h1>Bienvenido a Elearn</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+                provident labore sit placeat nihil aperiam architecto, quasi
+                aliquid est alias doloremque obcaecati molestias, a harum fugiat
+                voluptatem numquam dolor ipsa.
+            </p>
+            <app-button class="margin-top--one" :href="$route('login')" outline
+                >Empezar</app-button
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    //
+}
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
