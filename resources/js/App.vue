ecdf063920d876429369c381a243ec4b0b35f328
robinhood05 <robinhood0593@gmail.com>
2020-04-17 20:34:23 -0400

    Add some style & struncture to App.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,17 @@
 <template>
     <div id="app">
+        <!-- Header -->
+        <header class="navbar"></header>
+
+        <!-- Main -->
+        <main>
+            <div class="container">
+                <slot></slot>
+            </div>
+        </main>
+
+        <!-- Footer -->
+        <footer class="footer"></footer>
     </div>
 </template>
 
@@ -8,4 +20,38 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+$navbar-heigth: 55px;
+
+#app {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+}
+
+main {
+    padding-top: $navbar-heigth;
+    flex-grow: 1;
+
+    & > .container {
+        margin-top: get('two', $spacers);
+        @include breakpoint('desktop') {
+            margin-top: get('three', $spacers);
+        }
+    }
+}
+
+.navbar {
+    @include size(100%, $navbar-heigth);
+    @include position(fixed, 0, 0);
+
+    background: color('white');
+    border-bottom: 1px solid color('gray', 50);
+    z-index: get('navbar', $z-layers);
+}
+
+.footer {
+    height: 120px;
+    background: color('white');
+    border-top: 1px solid color('gray', 50);
+}
 </style>
