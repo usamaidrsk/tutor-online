d577be1567a3b8fb23830b5c200682ed8ce003c2
robinhood05 <robinhood0593@gmail.com>
2020-04-27 18:18:08 -0400

    Add relationship methods to Asigment Model

--- app/Asigment.php
+++ app/Asigment.php
@@ -8,6 +8,16 @@ class Asigment extends Model
 {
     public $guarded = ['id', 'created_at', 'updated_at'];
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
+
     public function files()
     {
         return $this->hasMany(File::class);
