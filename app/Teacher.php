b19639ee7fb34dc0e05b457f166caabfb27480f0
robinhood05 <robinhood0593@gmail.com>
2020-04-28 19:30:09 -0400

    Fix phone getter: now swallows stupid errors

--- app/Teacher.php
+++ app/Teacher.php
@@ -35,17 +35,20 @@ class Teacher extends Authenticatable
         return $this->first_name . ' ' . $this->last_name;
     }
 
-    public function getPhoneAttribute()
     public function getBirthdayAttribute()
     {
-        $phone = $this->attributes['phone'];
         $date = $this->attributes['birthday'];
         return Carbon::create($date)->isoFormat('D \d\e MMMM, YYYY');
     }
 
-        if ($phone) {
-            $phone = PhoneNumber::make($phone);
-            $phone = $phone->formatInternational();
+    public function getPhoneAttribute()
+    {
+        if ($phone = $this->attributes['phone']) {
+            try {
+                $phone = PhoneNumber::make($phone)->formatInternational();
+            } catch (\Throwable $th) {
+                report($th);
+            }
         }
 
         return $phone;
