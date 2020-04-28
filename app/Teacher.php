dcbccc8b5e79daa819da6df2b7f1e642bef7de0c
robinhood05 <robinhood0593@gmail.com>
2020-04-28 12:26:29 -0400

    Add `full_name` getter to Teacher model

--- app/Teacher.php
+++ app/Teacher.php
@@ -27,6 +27,11 @@ class Teacher extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
     public function getPhoneAttribute()
     {
         $phone = $this->attributes['phone'];
