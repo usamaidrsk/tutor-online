e86e35cecc88c31e6ca48d6eef2927d35fe15ee6
robinhood05 <robinhood0593@gmail.com>
2020-04-28 13:22:21 -0400

    Update design of TeacherCard

--- resources/js/components/TeacherCard.vue
+++ resources/js/components/TeacherCard.vue
@@ -1,15 +1,10 @@
 <template>
     <article class="card">
-        <div class="card__container">
-            <div class="card__body">
-                <figure class="card__image">
-                    <img :src="`/storage/${teacher.picture}`" />
-                </figure>
-                <h4 class="card__name">
-                    {{ teacher.first_name }} {{ teacher.last_name }}
-                </h4>
-                <Stars class="card__stars" :value="3" />
-            </div>
+        <div class="card__body">
+            <figure class="card__image">
+                <img :src="teacher.picture" />
+            </figure>
+            <Stars class="card__stars" :value="3" />
         </div>
     </article>
 </template>
@@ -27,16 +22,10 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-    width: 235px;
-    margin: 0 auto;
-    background: color('white');
-    border: 1px solid color('gray', 100);
-    border-radius: 5px;
-
+    text-align: center;
     transition-property: transform box-shadow;
     transition-duration: 0.3s;
     transition-timing-function: ease-out;
-    cursor: pointer;
 
     &:hover {
         transform: scale(1.1);
@@ -44,16 +33,13 @@ export default {
 }
 
 .card__body {
-    padding: 2rem 0 1rem;
-    text-align: center;
-}
-
-.card__name {
-    @include ellipsis;
-
-    margin: 0;
-    margin-top: 1rem;
-    margin-bottom: get('halve', $spacers);
+    display: inline-block;
+    padding: 1rem;
+    padding-bottom: 0.5rem;
+    background: color('white');
+    border-radius: 5px;
+    border: 1px solid color('gray', 50);
+    cursor: pointer;
 }
 
 .card__image {
