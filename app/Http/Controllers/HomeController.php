62439d43c580e12449ae42f9d15fa404562fb1dc
robinhood05 <robinhood0593@gmail.com>
2020-04-28 12:24:56 -0400

    Update HomeControlller to redirect to login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,7 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view()->component('home');
+        // return view()->component('home');
+        return redirect('login');
     }
 }
