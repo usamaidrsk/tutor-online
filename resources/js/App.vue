8a75d52fec653462e22ed2be7afa5c65d213138f
robinhood05 <robinhood0593@gmail.com>
2020-04-28 12:22:02 -0400

    Re-design navbar

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,7 +2,49 @@
     <div id="app">
         <!-- Header -->
         <header class="navbar">
-            <Button v-if="$auth.user" @click="doLogout" flat>Salir</Button>
+            <div class="navbar__container container">
+                <div class="navbar__left">
+                    <h1 class="navbar__logo">
+                        <a :href="route('home')">Elearn </a>
+                    </h1>
+                </div>
+
+                <div class="navbar__rigth">
+                    <template v-if="isLogged">
+                        <span class="navbar__wellcome"
+                            >Hola, {{ user.first_name }}</span
+                        >
+
+                        <figure class="navbar__avatar">
+                            <img :src="user.picture" />
+                        </figure>
+
+                        <!-- <a style="cursor: pointer;" @click="doLogout" flat
+                            >Salir</a
+                        > -->
+                    </template>
+                    <template v-else>
+                        <a
+                            class="navbar__button"
+                            :class="{
+                                'navbar__button--is-active': isCurrent(
+                                    'register'
+                                ),
+                            }"
+                            :href="route('register')"
+                            >Registrarse</a
+                        >
+                        <a
+                            class="navbar__button"
+                            :class="{
+                                'navbar__button--is-active': isCurrent('login'),
+                            }"
+                            :href="route('login')"
+                            >Iniciar</a
+                        >
+                    </template>
+                </div>
+            </div>
         </header>
 
         <!-- Main -->
@@ -19,11 +61,18 @@
 
 <script>
 export default {
+    data: ({ $auth }) => ({
+        user: $auth.user,
+        isLogged: !!$auth.user,
+    }),
+
     methods: {
         async doLogout() {
             await this.$http.post('logout')
             window.location.href = route('home')
         },
+
+        isCurrent: name => route().current() === route(name).name,
     },
 }
 </script>
@@ -51,13 +100,64 @@ main {
 }
 
 .navbar {
-    // @include elevation(3);
     @include size(100%, $navbar-heigth);
     @include position(fixed, 0, 0);
 
     background: color('white');
     border-bottom: 1px solid color('gray', 50);
     z-index: get('navbar', $z-layers);
+}
+
+.navbar__container {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.navbar__logo {
+    margin: 0;
+    font-size: 1.75rem;
+    text-transform: uppercase;
+}
+
+.navbar__rigth {
+    display: flex;
+    align-items: center;
+}
+
+.navbar__avatar {
+    @include size(35px);
+
+    background: color('gray', 50);
+    border-radius: 50%;
+    margin: 0 0.5rem;
+    overflow: hidden;
+}
+
+.navbar__button {
+    color: color('gray', 900);
+    text-transform: uppercase;
+    font-weight: get('regular', $font-weights);
+    margin-right: 1.85rem;
+
+    &::before {
+        @include size(10px);
+
+        content: ' ';
+        display: block;
+        float: left;
+        border-radius: 50%;
+        margin-top: 6px;
+        margin-right: 7px;
+        background: color('gray', 50);
+    }
+
+    &--is-active {
+        &::before {
+            background: color('primary');
+        }
+    }
 }
 
 .footer {
