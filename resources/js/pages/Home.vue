f438f266205d51a6131bdd11488beabf3036da02
robinhood05 <robinhood0593@gmail.com>
2020-04-28 12:24:26 -0400

    Delete content of Home.vue
    For now request to this page will redirect lo login.

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,17 +1,6 @@
 <template>
     <div>
-        <div class="hero text--center">
-            <h1>Bienvenido a Elearn</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
-                provident labore sit placeat nihil aperiam architecto, quasi
-                aliquid est alias doloremque obcaecati molestias, a harum fugiat
-                voluptatem numquam dolor ipsa.
-            </p>
-            <Button class="margin-top--one" :href="route('login')" outline
-                >Empezar</Button
-            >
-        </div>
+        <!--  -->
     </div>
 </template>
 
