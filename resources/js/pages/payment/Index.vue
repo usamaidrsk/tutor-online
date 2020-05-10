69716bbcc4d0c15190b2242237d2c5b25dbfea12
robinhood05 <robinhood0593@gmail.com>
2020-05-10 08:20:03 -0400

    Re-design payment pages

--- resources/js/pages/payment/Index.vue
+++ resources/js/pages/payment/Index.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
+    <div class="wrapper">
         <span v-if="error" class="error" role="alert">{{ error }}</span>
+
+        <header>
+            <h1 class="margin-bottom--one">Pago</h1>
+        </header>
+
+        <div class="total">
+            <h4 class="total__label">TOTAL</h4>
+            <h2 class="total__amount">${{ amount }}</h2>
+        </div>
 
         <form
             method="post"
@@ -9,9 +18,10 @@
         >
             <input type="hidden" name="_token" :value="CSRF_TOKEN" />
 
-            <h1>Pago</h1>
-            <h3>TOTAL: ${{ amount }}</h3>
-            <Button type="submit" :loading="loading">Pagar</Button>
+            <button class="paypal-button" type="submit" :disabled="loading">
+                <i class="icon icon-paypal"></i>
+                <span>PayPal</span>
+            </button>
         </form>
     </div>
 </template>
@@ -27,5 +37,40 @@ export default {
 <style lang="scss" scoped>
 @import '~@/sass/_globals.scss';
 
-//
+.wrapper {
+    text-align: center;
+}
+
+.total {
+    margin-top: 1.85rem;
+    margin-bottom: 2rem;
+}
+
+.total__label,
+.total__amount {
+    margin: 0;
+}
+
+.total__label {
+    font-weight: get('regular', $font-weights) !important;
+}
+
+.total__amount {
+    font-weight: get('light', $font-weights) !important;
+    font-size: 3.5rem !important;
+}
+
+.paypal-button {
+    background: #005ea6; // Paypal's blue
+    color: color('white');
+    padding: 1rem 3.5rem 1rem 3rem;
+    border-radius: 5px;
+    font-size: 1.1rem;
+
+    .icon {
+        font-size: 1.5rem;
+        vertical-align: top;
+        margin-right: 8px;
+    }
+}
 </style>
--- resources/js/pages/payment/Success.vue
+++ resources/js/pages/payment/Success.vue
@@ -1,14 +1,35 @@
 <template>
     <div class="wrapper padding-top--three text--center">
+        <div class="check">
+            <i class="icon icon-ok"></i>
+        </div>
+
         <h1 class="margin-bottom--zero">
             Tu pago se ha ejecutado correctamente!
         </h1>
         <p>Ahora solo queda esperar a que sea el dia y la hora de tu clase</p>
 
-        <Button class="margin-top--two">Continuar</Button>
+        <Button
+            class="margin-top--two"
+            color="green"
+            :href="route('asigment.index')"
+            >Continuar</Button
+        >
     </div>
 </template>
 
 <style lang="scss" scoped>
-//
+@import '~@/sass/_globals.scss';
+
+.check {
+    @include size(115px);
+    @include child-centered;
+
+    margin: 0 auto;
+    margin-bottom: 1rem;
+    border-radius: 50%;
+    color: color('green') !important;
+    font-size: 3.5rem;
+    border: 2px solid color('green');
+}
 </style>
