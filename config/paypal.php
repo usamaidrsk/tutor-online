a5882b9e64259c08af5049ef5ebb95f23926bcbe
robinhood05 <robinhood0593@gmail.com>
2020-05-18 11:20:18 -0400

    Change PayPal SDK log level

--- config/paypal.php
+++ config/paypal.php
@@ -8,7 +8,7 @@ return [
         'http.ConnectionTimeOut' => 0, //Max request time in seconds
         'log.LogEnabled' => true, //Whether want to log to a file
         'log.FileName' => storage_path() . '/logs/paypal.log', //Specify the file that want to write on
-        'log.LogLevel' => 'FINE', //Available option 'FINE', 'INFO', 'WARN' or 'ERROR'
+        'log.LogLevel' => 'ERROR', //Available option 'FINE', 'INFO', 'WARN' or 'ERROR'
         /**
          * Logging is most verbose in the 'FINE' level and decreases as you
          * proceed towards ERROR
