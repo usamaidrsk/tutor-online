5084e381a9cf6ec90cea80f64525c2a617f8a299
robinhood05 <robinhood0593@gmail.com>
2020-05-18 11:11:02 -0400

    Update HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use App\Asigment;
 
 class HomeController extends Controller
 {
@@ -10,6 +12,12 @@ class HomeController extends Controller
     {
         if (auth()->check()) {
             return redirect()->route('profile.index');
+        }
+
+        if ($email = Cookie::get('email')) {
+            if (Asigment::where('email', $email)->exists()) {
+                return redirect()->route('asigment.index');
+            }
         }
 
         return view()->component('home', ['title' => 'Inicio']);
